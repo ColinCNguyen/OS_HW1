a01e97961e404ef8cb2ea87ee89db640de42a137
ColinCNguyen <ColinCNguyen@hsutx.edu>
2016-02-03 12:55:53 -0600

    Have it currently finding all matches of a specific search method (Not using threads).

--- StudentDatabaseDriver.java
+++ StudentDatabaseDriver.java
@@ -11,16 +11,24 @@ public class StudentDatabaseDriver
   public static void main(String [] args) throws Exception
   {
     BookDatabase db= new BookDatabase("books.txt");  // be sure this file exists!
-    SearchRequest request= new SearchRequest(db,"Ben",0,db.size());  // prepare to search for "Ben"
-
-    db.display();
-    System.out.println("\n");
-
-    db.searchByTitle(request);
-    if (request.foundPos >= 0) {
-      System.out.println("Found matching title at position "+request.foundPos);
-      System.out.println("Title: "+db.getBook(request.foundPos));
-    }
+   // SearchRequest request= new SearchRequest(db,"567",0,db.size());  // prepare to search for "Ben"
+		SearchRequest request;	
+   // db.display();
+   // System.out.println("\n");
+   // db.searchByISBN(request);
+	 	int temp = 0;
+		for(int i=0; i<db.size(); i++){
+				request = new SearchRequest(db, "567", temp, db.size());
+				db.searchByISBN(request);
+				temp = request.foundPos;
+				if (request.foundPos >= 0) {
+					temp++;
+					System.out.println("Found matching title at position "+request.foundPos);
+					System.out.println("Title: "+db.getBook(request.foundPos));
+				}
+				else
+					i=db.size();
+		}
   }
 }
 
